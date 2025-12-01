b6cbfa985de6bfa19eeef26c71f784edd0f22549
Nemanja Stojanovic <feemail042@gmail.com>
2025-12-02 00:08:59 +0100

    utils function added

--- client/src/lib/utils.ts
+++ client/src/lib/utils.ts
@@ -6,6 +6,34 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
+export function formatEnumString(str: string) {
+  return str.replace(/([A-Z])/g, " $1").trim();
+}
+
+export function formatPriceValue(value: number | null, isMin: boolean) {
+  if (value === null || value === 0)
+    return isMin ? "Any Min Price" : "Any Max Price";
+  if (value >= 1000) {
+    const kValue = value / 1000;
+    return isMin ? `$${kValue}k+` : `<$${kValue}k`;
+  }
+  return isMin ? `$${value}+` : `<$${value}`;
+}
+
+export function cleanParams(params: Record<string, any>): Record<string, any> {
+  return Object.fromEntries(
+    Object.entries(params).filter(
+      (
+        [_, value] // eslint-disable-line @typescript-eslint/no-unused-vars
+      ) =>
+        value !== undefined &&
+        value !== "any" &&
+        value !== "" &&
+        (Array.isArray(value) ? value.some((v) => v !== null) : value !== null)
+    )
+  );
+}
+
 export async function createNewUserInDatabase(
   user: any,
   idToken: any,
